e7e7740aec824cd5c8d4b56060c5c558e0c0f1bc
aditya <adityasingh222247@gmail.com>
2019-04-04 22:33:00 +0530

    heroku commit

--- online_test/settings.py
+++ online_test/settings.py
@@ -71,13 +71,21 @@ WSGI_APPLICATION = 'online_test.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+import dj_database_url
 
-DATABASES = {
+DATABASE = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        dj_database_url.config(
+            default='postgres://drvgqeosbutunu:d5fa7859dc3115359827a12d34bd0f45ae71dbb330cdd56ff2caaf78a453ae47@ec2-184-73-210-189.compute-1.amazonaws.com:5432/d9uf3k3u90qnss'
+        )
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
